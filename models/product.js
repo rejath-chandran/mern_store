f8870315c76d71f6537294b8e5eb27424366bb67
rejath chandran <rejathchandran@rejaths-MacBook-Air.local>
2022-07-12 17:31:21 +0530

    added product schema

--- models/product.js
+++ models/product.js
@@ -1,13 +1,33 @@
 const mongoose=require("mongoose")
 const { Schema } = mongoose;
-
+const {ObjectId}= mongoose.Schema
 const ProductSchema= new Schema({
     name:{
         type:String,
         trim:true,
         unique:true
     },
-  
+    price:{
+        type:Number,
+        required:true,
+        maxlength:20,
+        trim:true
+    },
+    category:{
+        type:ObjectId,
+        ref:"category"
+    },
+    stock:{
+        type:Number
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+   photo:{
+    data:Buffer,
+    contentType:String
+    }
 },
 { timestamps:true}
 )
